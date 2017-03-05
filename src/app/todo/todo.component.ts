853d26a25c2b9a88664b0e037d9763cbf2fc0c3c
Maciej C <Maciej C>
2017-03-05 10:21:53 +0100

    Kod w js

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+todos: string[];
 
   constructor() { }
-
+  this.todos = ["Zrob to", "Zrob tamto", "Zrob cos jeszcze"];
   ngOnInit() {
   }
 
